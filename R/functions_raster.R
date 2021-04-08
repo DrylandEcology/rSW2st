51d08a72e5debe8292b00c70ee4a849da8b3655d
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2021-04-08 15:48:57 -0400

    Move function `get_isoline_polygon()` from `rSW2analysis`
    - moved code as is

--- R/functions_raster.R
+++ R/functions_raster.R
@@ -171,3 +171,36 @@ get_raster_datatype <- function(data) {
     DOUBL = "FLT8S"
   )
 }
+
+
+#' Creates an isoline-polygon from data where values are larger than alpha
+#'
+#' Function converts data to a raster object and then draws a
+#' polygon around those gridcells with a value larger than alpha
+#'
+#' @param x A numeric vector
+#' @inheritParams create_raster_from_variables
+#' @param subset A logical vector of length equal to \code{x}
+#' @param alpha A numeric value
+#'
+#' @return A \code{\link[sp]{SpatialPolygons}} object.
+#'
+#' @export
+get_isoline_polygon <- function(x, site_locations, grid, subset, alpha) {
+  if (!missing(subset)) {
+    x[!subset] <- NA
+  }
+
+  rtmp <- create_raster_from_variables(
+    data = x,
+    site_locations = site_locations,
+    grid = grid
+  )
+
+  rtmp <- raster::calc(
+    rtmp,
+    fun = function(x) ifelse(x >= alpha, 1L, NA)
+  )
+
+  raster::rasterToPolygons(rtmp, dissolve = TRUE)
+}
