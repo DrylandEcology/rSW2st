3c07fd3046a4bb8c245929268a4da83543e58da6
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2021-04-15 15:00:09 -0400

    Improve function `variogram_range()`
    - Updated documentation and added code examples
    - Added unit tests
    
    - function can now accept either a raster or a set of points as input

--- R/functions_spatial.R
+++ R/functions_spatial.R
@@ -1,28 +1,63 @@
 
-#' Calculate range of variogram in kilometers
+#' Automatic calculation of a variogram range
 #'
 #' @param x A \code{\link[raster:RasterLayer-class]{raster::RasterLayer}}
-#'   in \var{un}-projected coordinates (i.e., longitude/latitude)
+#'   or an object that can be passed to \code{\link{as_points}}.
+#' @param project_to_utm A logical value. If \code{TRUE}, then \code{x}
+#'   will be projected to a suitable \var{UTM}; otherwise, \code{x} will
+#'   be projected to \var{WGS84}.
+#' @param sub_samplepoints_N An integer value. If not \code{NULL},
+#'   then a random sample of \code{x} will be taken of the specified size.
+#' @inheritParams as_points
+#' @param seed Passed to \code{\link{set.seed}}; used only if
+#'   \code{sub_samplepoints_N} is a number.
+#'
+#' @seealso \code{\link[automap]{automap::autofitVariogram}}
+#'
+#' @return A numeric value representing the variogram range
+#'   in units of \code{x}.
+#'
+#' @examples
+#' xy <- as_points(0.5 + cbind(0:9, 0:9), crs = 6350, to_class = "sf")
+#' variogram_range(xy)
 #'
 #' @export
-variogram_range <- function(x, project_to_utm = TRUE,
-  sub_samplepoints_N = NULL) {
+variogram_range <- function(
+  x,
+  project_to_utm = TRUE,
+  sub_samplepoints_N = NULL,
+  crs,
+  seed = NULL
+) {
 
-  stopifnot(requireNamespace("sp"), requireNamespace("raster"),
-    requireNamespace("automap"), requireNamespace("gstat"))
+  stopifnot(
+    requireNamespace("automap"),
+    requireNamespace("gstat")
+  )
 
-  # Create spatial points from raster
-  points <- raster::rasterToPoints(x, spatial = TRUE)
+  if (inherits(x, "RasterLayer")) {
+    points <- raster::rasterToPoints(x, spatial = TRUE)
+    names(points) <- "target"
+
+  } else {
+    tmp <- as_points(x, crs = crs, to_class = "sp")
+    points <- sp::SpatialPointsDataFrame(
+      coords = tmp,
+      data = data.frame(target = rep(1, length(tmp)))
+    )
+  }
 
   if (!is.null(sub_samplepoints_N)) {
-    set.seed(2017)
-    temp <- sample(seq_len(nrow(points)), sub_samplepoints_N,
-      replace = FALSE)
-    points <- points[temp, ]
+    set.seed(seed)
+    tmp <- sample(
+      seq_len(nrow(points)),
+      sub_samplepoints_N,
+      replace = FALSE
+    )
+    points <- points[tmp, ]
   }
 
   if (project_to_utm) {
-    # if region, then project to UTM coordinates with distance units of km
     # --> variogram will calculate Euclidean distances in map units
     points_prj <- sp::spTransform(
       points,
@@ -30,23 +65,22 @@ variogram_range <- function(x, project_to_utm = TRUE,
     )
 
   } else {
-    # if global, then don't project, i.e. use long/lat with WGS84 datum
-    # and distance units of meters
-    # --> variogram will calculate great circle distances in kilometers(!)
+    # --> variogram will calculate great circle distances(!)
     points_prj <- sp::spTransform(
       points,
       CRSobj = as(sf::st_crs(4326), "CRS")
     )
   }
 
-  names(points_prj) <- "target"
 
-  # determine variogram; see gstat::variogram
-  fittedVar <- automap::autofitVariogram(target ~ 1,
+  #--- determine variogram; see gstat::variogram
+  fittedVar <- automap::autofitVariogram(
+    target ~ 1,
     input_data = points_prj,
-    miscFitOptions = list(merge.small.bins = TRUE))
+    miscFitOptions = list(merge.small.bins = TRUE)
+  )
 
-  # variogram range in km:
+  #--- variogram range
   # estimate is only valid if some variation in data (i.e., sill > 0)
   psill_sum <- sum(fittedVar[["var_model"]][, "psill"])
 
---
+++ tests/testthat/test_spatial.R
@@ -0,0 +1,31 @@
+test_that("variogram_range", {
+  r <- raster::raster(
+    xmn = 0, xmx = 10,
+    ymn = 0, ymx = 10,
+    crs ="EPSG:6350",
+    resolution = c(1, 1)
+  )
+
+  xy <- as_points(
+    0.5 + cbind(0:9, 0:9),
+    crs = 6350,
+    to_class = "sf"
+  )
+
+  rv1 <- create_raster_from_variables(
+    data = rep(1, 10),
+    site_locations = xy,
+    grid = r
+  )
+
+  expect_equal(
+    variogram_range(x = rv1),
+    variogram_range(x = xy)
+  )
+
+  expect_equal(
+    variogram_range(x = rv1, sub_samplepoints_N = 5, seed = 2017),
+    variogram_range(x = xy, sub_samplepoints_N = 5, seed = 2017),
+    tolerance = 1e-6
+  )
+})
